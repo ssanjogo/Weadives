39ea6b281910515302a389c37af1245b0abaf800
oscaroca <oscarocaub@gmail.com>
2022-04-26 18:37:20 +0200

    US-FE-54. Cambiar la interfaz de parametros a publicaciones en "Mi perfil".

--- app/src/main/java/com/example/weadives/PantallaMiPerfil/PantallaMiPerfil.java
+++ app/src/main/java/com/example/weadives/PantallaMiPerfil/PantallaMiPerfil.java
@@ -22,10 +22,13 @@ import com.example.weadives.DatoGradosClass;
 import com.example.weadives.Directions;
 import com.example.weadives.PantallaInicio.PantallaInicio;
 import com.example.weadives.PantallaLogIn.PantallaLogIn;
+import com.example.weadives.PantallaPerfilAmigo.PublicacionClass;
+import com.example.weadives.PantallaPerfilAmigo.PublicacionesPerfilAdapter;
 import com.example.weadives.ParametrosClass;
 import com.example.weadives.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PantallaMiPerfil extends AppCompatActivity {
@@ -37,7 +40,7 @@ public class PantallaMiPerfil extends AppCompatActivity {
     private DatabaseAdapter dbA;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private List<ParametrosClass> parametrosList;
+    //private List<ParametrosClass> parametrosList;
 
 
     @Override
@@ -51,7 +54,8 @@ public class PantallaMiPerfil extends AppCompatActivity {
         btn_config = findViewById(R.id.btn_config);
         btn_cerrarSesion = findViewById(R.id.btn_cerrarSession);
 
-        parametrosList = fillParametrosList();
+        //parametrosList = fillParametrosList();
+        List<PublicacionClass> publicacionesList= fillPublicacionList();
 
         recyclerView = findViewById(R.id.rv_llistaAjustes);
         //mejorar performance
@@ -60,7 +64,7 @@ public class PantallaMiPerfil extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         //especificar adapter
-        mAdapter= new ParametrosPerfilAdapter(parametrosList,PantallaMiPerfil.this);
+        mAdapter= new PublicacionesPerfilAdapter(publicacionesList,PantallaMiPerfil.this);
         recyclerView.setAdapter(mAdapter);
 
         Intent intent = getIntent();
@@ -110,5 +114,45 @@ public class PantallaMiPerfil extends AppCompatActivity {
         parametrosList.add(new ParametrosClass("BalonPie",  0.2f,0.1f,0.3f,0.2f,0.3f,0.3f, new DatoGradosClass(Directions.SUD),3.f,2.f,4.f,4.f,new DatoGradosClass(Directions.ESTE)));
         parametrosList.add(new ParametrosClass("DokkanBattle",  0.2f,0.1f,0.3f,0.2f,0.3f,0.3f, new DatoGradosClass(Directions.SUD),3.f,2.f,4.f,4.f,new DatoGradosClass(Directions.ESTE)));
         return parametrosList;
+    }
+    private List<PublicacionClass> fillPublicacionList() {
+        List<PublicacionClass> publicacionList= new ArrayList<PublicacionClass>();
+        //String nombreActividad, int userID, float presionMax, float presionMin, float temperaturaMax, float temperaturaMin, float vientoMax, float vientoMin, DatoGradosClass directionViento, float alturaOlaMax, float alturaOlaMin, float periodoOlaMax, float periodoOlaMin, DatoGradosClass directionOlas
+        ParametrosClass p1= new ParametrosClass("SurfLoco",  0.2f,0.1f,0.3f,0.2f,0.3f,0.3f, new DatoGradosClass(Directions.NORTE),3.f,2.f,4.f,4.f,new DatoGradosClass(Directions.SUD));
+        HashMap<String, Integer> likeList1=new HashMap<>();
+        likeList1.put("0000",1);
+        likeList1.put("0001",1);
+        likeList1.put("0002",0);
+        HashMap<String, String> comentariosList1=new HashMap<>();
+        comentariosList1.put("Carlos Chun","Oye, ta guapo etooo");
+        comentariosList1.put("Oscaroca","Esta guay para disfrutar del restaurante que hay al lado.");
+        comentariosList1.put("Mi pana miguel","Viva er beti");
+        comentariosList1.put("Racsor","Fumas?");
+        comentariosList1.put("Mikol","Jo parlo catalá");
+        comentariosList1.put("Sara","Totorooooo");
+        comentariosList1.put("Matt","Has visto como entrenar a tu dragon?");
+        comentariosList1.put("Septimus","AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        publicacionList.add(new PublicacionClass(p1,likeList1,comentariosList1));
+        ParametrosClass p2= new ParametrosClass("Surf",  0.2f,0.1f,0.3f,0.2f,0.3f,0.3f, new DatoGradosClass(Directions.SUD),3.f,2.f,4.f,4.f,new DatoGradosClass(Directions.ESTE));
+        HashMap<String, Integer> likeList2=new HashMap<>();
+        likeList2.put("0000",0);
+        likeList2.put("0001",0);
+        likeList2.put("0002",0);
+        HashMap<String, String> comentariosList2=new HashMap<>();
+        comentariosList2.put("0001","---");
+        comentariosList2.put("0021","---");
+        comentariosList2.put("0031","---");
+        publicacionList.add(new PublicacionClass(p2,likeList2,comentariosList2));
+        ParametrosClass p3= new ParametrosClass("DokkanBattle", 0.2f,0.1f,0.3f,0.2f,0.3f,0.3f, new DatoGradosClass(Directions.SUD),3.f,2.f,4.f,4.f,new DatoGradosClass(Directions.ESTE));
+        HashMap<String, Integer> likeList3=new HashMap<>();
+        likeList3.put("0000",1);
+        likeList3.put("0001",1);
+        likeList3.put("0002",1);
+        HashMap<String, String> comentariosList3=new HashMap<>();
+        comentariosList3.put("0001","---");
+        comentariosList3.put("0021","---");
+        publicacionList.add(new PublicacionClass(p3,likeList3,comentariosList3));
+
+        return publicacionList;
     }
 }
